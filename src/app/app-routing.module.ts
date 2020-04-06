d0f1a19d367ed2b6e44a08e67c2c17193b3c5869
Eider Arley Muñoz <eider205v@gmail.com>
2020-04-06 06:47:42 -0500

    EAMV Configuracion inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AgregarComponent} from './componentes/atencion/agregar/agregar.component';
+import {ListarAtencionComponent} from './componentes/atencion/listar-atencion/listar-atencion.component';
+import {ListarIngredienteComponent} from './componentes/ingrediente/listar-ingrediente/listar-ingrediente.component';
+import {ListarPlatoComponent} from './componentes/plato/listar-plato/listar-plato.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'agregar_atencion', component: AgregarComponent},
+  {path: 'listar_atencion', component: ListarAtencionComponent},
+  {path: 'listar_ingrediente', component: ListarIngredienteComponent},
+  {path: 'listar_plato', component: ListarPlatoComponent},
+  {path: '', redirectTo: '/listar_ingrediente', pathMatch: 'full'},
+  {path: '**', redirectTo: '/listar_ingrediente', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './componentes/app/app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { AgregarComponent } from './componentes/atencion/agregar/agregar.component';
+import { ListarAtencionComponent } from './componentes/atencion/listar-atencion/listar-atencion.component';
+import { ListarIngredienteComponent } from './componentes/ingrediente/listar-ingrediente/listar-ingrediente.component';
+import { ListarPlatoComponent } from './componentes/plato/listar-plato/listar-plato.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    AgregarComponent,
+    ListarAtencionComponent,
+    ListarIngredienteComponent,
+    ListarPlatoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
