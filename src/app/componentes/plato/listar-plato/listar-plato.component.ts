47cc398397b151a4497318581748a2e83dea50f7
Eider Arley Muñoz <eider205v@gmail.com>
2020-04-06 10:44:55 -0500

    EAMV Implementacion de sede servicio

--- src/app/componentes/plato/listar-plato/listar-plato.component.ts
+++ src/app/componentes/plato/listar-plato/listar-plato.component.ts
@@ -4,6 +4,8 @@ import {MatTableDataSource} from '@angular/material/table';
 import {MatPaginator} from '@angular/material/paginator';
 import {Router} from '@angular/router';
 import {PlatoService} from '../../../servicios/plato.service';
+import {Sede} from "../../../modelos/sede";
+import {SedeService} from "../../../servicios/sede.service";
 
 @Component({
   selector: 'app-listar-plato',
@@ -12,8 +14,10 @@ import {PlatoService} from '../../../servicios/plato.service';
 })
 export class ListarPlatoComponent implements OnInit {
 
+  private sedes: Sede[];
   elementos: Plato[] = [];
-// id_producto, nombre, porcentaje_ganancia, precio, cantidad, id_categoria
+  selected = '1';
+// total, plato, ubicacion
   displayedColumns: string[] = ['total', 'plato', 'ubicacion'];
   dataSource: MatTableDataSource<Plato>;
   // creacion de paginacion de la tabla
@@ -26,24 +30,32 @@ export class ListarPlatoComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private platoService: PlatoService
+    private platoService: PlatoService,
+    private sedeService: SedeService
   ) {
   }
 
   ngOnInit() {
 
-    this.platoService.getPlatos(1);
+    this.sedeService.getSedes();
 
-    this.cargarPlatos();
+    this.cargarSedes();
   }
 
-  cargarPlatos() {
+  cargarPlatos(idS: string) {
+    this.platoService.getPlatos(Number(idS));
     setTimeout(() => {
       this.elementos = this.platoService.mostrarPlatos();
       // console.log('elementos ' + this.elementos);
       this.dataSource = new MatTableDataSource(this.elementos);
       // paginacion de la tabla
       this.dataSource.paginator = this.paginacion;
+    }, 500);
+  }
+
+  cargarSedes() {
+    setTimeout(() => {
+      this.sedes = this.sedeService.getSedes();
     }, 500);
 
   }
