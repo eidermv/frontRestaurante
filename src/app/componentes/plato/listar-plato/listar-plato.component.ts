b47bc03e90ae4fe6f07898b419898aa7b16fd5b8
Eider Arley Muñoz <eider205v@gmail.com>
2020-04-06 06:49:39 -0500

    EAMV Creacion de plato

---
+++ src/app/componentes/plato/listar-plato/listar-plato.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-plato',
+  templateUrl: './listar-plato.component.html',
+  styleUrls: ['./listar-plato.component.css']
+})
+export class ListarPlatoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
